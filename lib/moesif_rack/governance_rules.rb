7b8ae89661ada8a48b42b8b7434f2d30d3c10f55
xinghengwang <xing@moesif.com>
2023-06-27 18:59:42 -0400

    updated logic for rules express

--- lib/moesif_rack/governance_rules.rb
+++ lib/moesif_rack/governance_rules.rb
@@ -5,6 +5,91 @@ require 'stringio'
 require_relative './moesif_helpers'
 require_relative './regex_config_helper'
 
+# rule refereence
+# {
+#   "_id": "649b64ea96d5e2384e3cece6",
+#   "created_at": "2023-06-27T22:38:34.405",
+#   "type": "regex",
+#   "state": 2,
+#   "org_id": "688:25",
+#   "app_id": "768:74",
+#   "name": "teset govern rule. ",
+#   "block": true,
+#   "applied_to": "matching",
+#   "applied_to_unidentified": false,
+#   "response": {
+#       "status": 205,
+#       "headers": {
+#           "X-Test": "12423"
+#       },
+#       "body": {
+#           "hello": "there"
+#       }
+#   },
+#   "regex_config": [
+#       {
+#           "conditions": [
+#               {
+#                   "path": "request.route",
+#                   "value": "test"
+#               },
+#               {
+#                   "path": "request.verb",
+#                   "value": "test"
+#               }
+#           ]
+#       },
+#       {
+#           "conditions": [
+#               {
+#                   "path": "request.ip_address",
+#                   "value": "teset"
+#               },
+#               {
+#                   "path": "request.verb",
+#                   "value": "5"
+#               }
+#           ]
+#       }
+#   ]
+# }
+
+# user rule reference.
+
+# {
+#   "_id": "649b65d83a5a0131fd035427",
+#   "created_at": "2023-06-27T22:42:32.301",
+#   "type": "user",
+#   "state": 2,
+#   "org_id": "688:25",
+#   "app_id": "768:74",
+#   "name": "test user rule",
+#   "block": false,
+#   "applied_to": "matching",
+#   "applied_to_unidentified": true,
+#   "response": {
+#       "status": 200,
+#       "headers": {
+#           "teset": "test"
+#       }
+#   },
+#   "cohorts": [
+#       {
+#           "id": "645c3793cba73323bb0760e6"
+#       }
+#   ],
+#   "regex_config": [
+#       {
+#           "conditions": [
+#               {
+#                   "path": "request.verb",
+#                   "value": "get"
+#               }
+#           ]
+#       }
+#   ]
+# }
+
 module APPLY_TO
   MATCHING = :matching
   NOT_MATCHING = :not_matching
@@ -87,44 +172,57 @@ class GovernanceRules
     get_rules(api_controller)
   end
 
-  #TODO
+  # TODO
   def convert_uri_to_route(uri); end
 
-  def prepare_request_config_based_on_regex_config(_env, _event_model)
-    regex_config = {
+  def prepare_request_fields_based_on_regex_config(_env, _event_model)
+    field_values = {
       'request.verb': _event_model.dig('request', 'verb'),
       'request.ip_address': _event_model.dig('request', 'ip_address'),
-      'request.route': convert_uri_to_route(_event_model.dig('request', 'uri'))
+      'request.route': convert_uri_to_route(_event_model.dig('request', 'uri')),
       'request.body.operationName': _event_model.dig('request', 'body', 'operationName')
     }
 
+    # TODO: for body
     request_body = _event_model.dig('request', 'body')
 
-    if request_body
-      regex_config['request.body'] = request_body
-    end
+    field_values['request.body'] = request_body if request_body
 
-    regex_config
+    field_values
   end
 
-  def check_request_with_regex_math(regex_config, request_fields, request_body)
+  def check_request_with_regex_math(regex_configs, request_fields, _request_body)
+    array_to_or = regex_configs.map do |or_group_of_regex_rule|
+      conditions = or_group_of_regex_rule.fetch('conditions', [])
 
+      conditions.reduce(true) do |all_match, condition|
+        return false unless all_match
+
+        path = condition.fetch('path')
+        field_value = request_fields.fetch(path)
+        reg_ex = Regexp.new condition.fetch('value')
+
+        field_value =~ reg_ex
+      end
+    end
+
+    array_to_or.reduce(false) { |anysofar, curr| anysofar || curr }
   end
 
   def get_rule_ids_if_governance_rule_matched(_env, _event_model)
     request_fields = prepare_request_fields_based_on_regex_config(_env, _event_model)
-    #FIXME
+    # FIXME
     rquest_body = _event_model.dig('request', 'body')
 
-    matched_rules = @regex_rules.select { |rule|
-      regex_config = rule.fetch('regex_config')
+    matched_rules = @regex_rules.select do |rule|
+      regex_configs = rule.fetch('regex_config')
       return false unless regex_config
 
-      matched = check_request_with_regex_match(regex_config, request_fields, request_body)
+      matched = check_request_with_regex_match(regex_configs, request_fields, request_body)
       matched
-    }
+    end
 
-    matched_rules.map {|rule| rule['_id']}
+    matched_rules.map { |rule| rule['_id'] }
   end
 
   def apply_regex_rules(_config, _env, _event_model)
@@ -141,7 +239,6 @@ class GovernanceRules
     # Priority is user rule, company rule, and regex.
     # by apply in reverse order, the last rule is highest priority.
 
-
     company_id_matched_rule_ids = _config.dig('company_rules', _company_id)
     unless company_id_matched_ruled_ids.nil?
       # apply compnay rule.
@@ -151,8 +248,6 @@ class GovernanceRules
     unless user_id_matched_rule_ids.nil?
       # apply user rule
     end
-
-
 
     [block, new_status, additional_headers, new_body]
   end
