229e99004de10a4c16dd6e2478a9a374b2ea1c58
xinghengwang <xing@moesif.com>
2023-07-18 13:12:15 -0700

    no need for decompress body since faraday already does it

--- lib/moesif_rack/moesif_helpers.rb
+++ lib/moesif_rack/moesif_helpers.rb
@@ -12,30 +12,6 @@ class MoesifHelpers
     puts("#{Time.now} [Moesif Middleware] PID #{Process.pid} TID #{Thread.current.object_id} #{message}")
   end
 
-  def decompress_gzip_body(moesif_api_response)
-    # Decompress gzip response body
-
-    # Check if the content-encoding header exist and is of type zip
-    if moesif_api_response.headers.key?(:content_encoding) && moesif_api_response.headers[:content_encoding].eql?('gzip')
-
-      # Create a GZipReader object to read data
-      gzip_reader = Zlib::GzipReader.new(StringIO.new(moesif_api_response.raw_body.to_s))
-
-      # Read the body
-      uncompressed_string = gzip_reader.read
-
-      # Return the parsed body
-      JSON.parse(uncompressed_string)
-    else
-      @moesif_helpers.log_debug 'Content Encoding is of type other than gzip, returning nil'
-      nil
-    end
-  rescue StandardError => e
-    @moesif_helpers.log_debug 'Error while decompressing the response body'
-    @moesif_helpers.log_debug e.to_s
-    nil
-  end
-
   def format_replacement_body(replacement_body, original_body)
     # replacement_body is an hash or array json in this case.
     # but original body could be in chunks already. we want to follow suit.
